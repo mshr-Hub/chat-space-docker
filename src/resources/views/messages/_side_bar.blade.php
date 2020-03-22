4ec3339a2866855ae63d05ea47f2c3f1bcc71ff2
mshr-Hub <masa.megane.616@gmail.com>
2020-03-22 22:18:06 +0900

    update: サイドバーのヘッダーを作成

--- src/resources/views/messages/_side_bar.blade.php
+++ src/resources/views/messages/_side_bar.blade.php
@@ -1,4 +1,23 @@
 <div class="side-bar">
-    <div class="side-bar__header"></div>
+    <div class="side-bar__header">
+        <div class="user-info">
+            <div class="user-info__name">
+                masa
+            </div>
+            <ul class="user-info__menu">
+                <li class="new-group">
+                    <a href="#">
+                        <i class="fa fa-edit"></i>
+                    </a>
+                </li>
+                <li class="edit-user">
+                    <a href="#">
+                        <i class="fa fa-cog"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
     <div class="side-bar__body"></div>
 </div>
