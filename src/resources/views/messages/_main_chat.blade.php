981bac6331f9d5e5e56098c21f4b5842de4e56a3
mshr-Hub <masa.megane.616@gmail.com>
2020-03-23 22:45:12 +0900

    update: メイン画面のグループ概要

--- src/resources/views/messages/_main_chat.blade.php
+++ src/resources/views/messages/_main_chat.blade.php
@@ -1,13 +1,19 @@
 <div class="main-chat">
     <div class="main-chat__header">
         <div class="left">
-            <div class="left__group">グループ名</div>
+            <h1 class="left__group">グループ名</h1>
             <ul class="left__member">
                 Member:
-                <li class="left__member__name">メンバー名</li>
+                <li class="left__member__name">メンバー1</li>
+                <li class="left__member__name">メンバー2</li>
+                <li class="left__member__name">メンバー3</li>
             </ul>
         </div>
-        <div class="right"></div>
+        <a class="right" href="#">
+            <div class="right__edit-btn">
+                Edit
+            </div>
+        </a>
     </div>
     <div class="main-chat__body"></div>
     <div class="main-chat__message"></div>
