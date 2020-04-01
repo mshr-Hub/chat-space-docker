8e4be13b295e81191c896ad4b659ef7623e5d2a2
mshr-Hub <masa.megane.616@gmail.com>
2020-04-02 01:23:37 +0900

    update: メッセージ一覧を作成

--- src/resources/views/messages/_main_chat.blade.php
+++ src/resources/views/messages/_main_chat.blade.php
@@ -1,20 +1,54 @@
 <div class="main-chat">
     <div class="main-chat__header">
-        <div class="left">
-            <h1 class="left__group">グループ名</h1>
-            <ul class="left__member">
+        <div class="summary">
+            <h1 class="summary__group">グループ名</h1>
+            <ul class="summary__member">
                 Member:
-                <li class="left__member__name">メンバー1</li>
-                <li class="left__member__name">メンバー2</li>
-                <li class="left__member__name">メンバー3</li>
+                <li class="summary__member__name">メンバー1</li>
+                <li class="summary__member__name">メンバー2</li>
+                <li class="summary__member__name">メンバー3</li>
             </ul>
         </div>
-        <a class="right" href="#">
-            <div class="right__edit-btn">
+        <a class="rbtn" href="#">
+            <div class="rbtn__edit">
                 Edit
             </div>
         </a>
     </div>
-    <div class="main-chat__body"></div>
-    <div class="main-chat__message"></div>
+    <div class="main-chat__body">
+        <div class="message-list">
+            <div class="message">
+                <div class="message__header">
+                    <p class="message__header__user-name">masa</p>
+                    <p class="message__header__sending-time">2020/4/1(Wed) 12:00:00</p>
+                </div>
+                <div class="message__body">
+                    <p>こんにちは！</p>
+                </div>
+            </div>
+            <div class="message">
+                <div class="message__header">
+                    <p class="message__header__user-name">masa</p>
+                    <p class="message__header__sending-time">2020/4/1(Wed) 13:00:00</p>
+                </div>
+                <div class="message__body">
+                    <p>働いてえらい！</p>
+                </div>
+            </div>
+            <div class="message">
+                <div class="message__header">
+                    <p class="message__header__user-name">masa</p>
+                    <p class="message__header__sending-time">2020/4/1(Wed) 14:00:00</p>
+                </div>
+                <div class="message__body">
+                    <p>
+                        今日も<br>
+                        働いて<br>
+                        えらい！
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="main-chat__form"></div>
 </div>
