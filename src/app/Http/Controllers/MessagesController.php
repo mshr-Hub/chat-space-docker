8aab148d87e1a7e3332bee26325322a04fffdc2c
mshr-Hub <masa.megane.616@gmail.com>
2020-04-07 00:25:26 +0900

    update: メッセージコントローラから不要なアクションメソッドを削除

--- src/app/Http/Controllers/MessagesController.php
+++ src/app/Http/Controllers/MessagesController.php
@@ -27,39 +27,6 @@ class MessagesController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
