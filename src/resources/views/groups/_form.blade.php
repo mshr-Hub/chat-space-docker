8cc1edb658870632a59d44102a90ed51e48b82ea
mshr-Hub <masa.megane.616@gmail.com>
2020-04-28 00:31:01 +0900

    update: グループ作成フォームを作成

--- src/resources/views/groups/_form.blade.php
+++ src/resources/views/groups/_form.blade.php
@@ -1,6 +1,6 @@
-<div class="container">
+<div class="container py-5">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <!-- TODO: createとeditで表示するテキストを変更させる -->
                 <div class="card-header">{{ __('Create Group') }}</div>
@@ -12,7 +12,7 @@
                         <div class="form-group row">
                             <label for="group__name" class="col-md-4 col-form-label text-md-right">{{ __('Group Name') }}</label>
                             <div class="col-md-6">
-                                <input id="group__name" type="text" class="form-control @error('group__name') is-invalid @enderror" name="group__name" required autofocus>
+                                <input id="group__name" type="text" class="form-control @error('group__name') is-invalid @enderror" name="groups[name]" placeholder="グループ名を入力して下さい" required autofocus>
                                 @error('group__name')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -24,12 +24,18 @@
                         <div class="form-group row">
                             <label for="user__index" class="col-md-4 col-form-label text-md-right">{{ __('User Search') }}</label>
                             <div class="col-md-6">
-                                <input id="user__index" type="text" class="form-control @error('user__index') is-invalid @enderror" required autofocus>
+                                <input id="user__index" type="text" class="form-control" placeholder="追加したいユーザー名を入力して下さい">
                             </div>
                         </div>
 
-                        <!-- TODO: hidden_fieldでユーザー情報を追加 -->
-                        <!-- TODO: 複数のユーザーを追加出来るようにname属性に"[]"を付ける -->
+                        <div class="form-group row">
+                            <label for="group__user" class="col-md-4 col-form-label text-md-right">{{ __('Group User') }}</label>
+                            <div class="col-md-6">
+                                <input id="group__user__{{ $user->id }}" type="hidden" class="form-control" value="{{ $user->id }}" name="group_user[user_id][]" required>
+                                <label for="group__user__{{ $user->id }}" class="col-form-label">{{ $user->name }}</label>
+                            </div>
+                            <!-- TODO: hidden_fieldでユーザー情報を追加 -->
+                        </div>
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
